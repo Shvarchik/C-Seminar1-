d16598ccd78806ca4e7ea10e6cbf9d292d03b5cd
Inna <shvartz_i@mail.ru>
2021-12-23 05:02:30 +0300

    Task 4_5 added

---
+++ Task4_5_DigitsOfNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+string Number = Console.ReadLine();
+int ABC = int.Parse(Number);
+int C = ABC % 10;
+int B = (ABC % 100 - ABC % 10) / 10;
+// вариант без %:
+//int A = ABC /100;
+//A = A * 100;
+//int B = (ABC - A)/10;
+//int C = ABC - A - B*10;
+Console.WriteLine ("Вторая цифра числа:" + B);
+Console.WriteLine ("Третья цифра числа:" + C);
