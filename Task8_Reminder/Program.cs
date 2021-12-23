0fa284222e7d9ccab5c3390b69ec8cd21c33ee64
Inna <shvartz_i@mail.ru>
2021-12-23 06:13:03 +0300

    Task8 added

---
+++ Task8_Reminder/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число:");
+string Number1 = Console.ReadLine();
+int num1 = int.Parse(Number1);
+Console.Write("Введите второе число:");
+string Number2 = Console.ReadLine();
+int num2 = int.Parse(Number2);
+if (num1 % num2 == 0)
+{
+    Console.WriteLine ("{0} кратно {1}", num1, num2);
+}
+else
+{
+    Console.WriteLine("{0} не кратно {1}, остаток от деления {2}", num1, num2, num1 % num2);
+}
