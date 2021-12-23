dc499f31d17a19b7e694b1da3e253bd44b89769f
Inna <shvartz_i@mail.ru>
2021-12-23 04:04:14 +0300

    Task2 added

---
+++ Task2_WeekDay/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] Week = {"понедельник", "вторник","среда", "четверг", "пятница", "суббота", "воскресенье"};
+Console.Write("Введите номер дня недели:");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+Console.WriteLine (num + "-й день недели - " + Week[num-1]);
