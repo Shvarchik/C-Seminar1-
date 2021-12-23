f9a0b2064f369b394e2375ba4a975392778e3731
Inna <shvartz_i@mail.ru>
2021-12-23 05:59:11 +0300

    Task6 added

---
+++ Task6_HighDigit/Program.cs
@@ -0,0 +1,12 @@
+﻿int AB = new Random().Next(10, 100);
+Console.WriteLine("Выбрано число " + AB);
+int B = AB % 10;
+int A = AB / 10;
+if (A > B)
+{
+  Console.WriteLine ("Наибольшая цифра числа " + A);
+}
+else
+{
+   Console.WriteLine ("Наибольшая цифра числа " + B);
+}
