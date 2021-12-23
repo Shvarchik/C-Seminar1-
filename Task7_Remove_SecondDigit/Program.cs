85ea28a4354c5749c65c8f62f984df7cc4567fde
Inna <shvartz_i@mail.ru>
2021-12-23 05:20:48 +0300

    Task7 added

---
+++ Task7_Remove_SecondDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число: ");
+string Number = Console.ReadLine();
+int ABC = int.Parse(Number);
+  //int C = ABC % 10;
+  //int A = (ABC / 100) * 100;
+  //int AC = A + C;
+int AC = ABC / 100 * 100 + ABC % 10;
+Console.WriteLine ("Без второй цифры это число:" + AC);
