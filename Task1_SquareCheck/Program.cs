092d8262fbee0a4674d359b6b5296c1deb9e820b
Inna <shvartz_i@mail.ru>
2021-12-23 03:36:58 +0300

    Task1 added

--- Task1_SquareCheck/Program.cs
+++ Task1_SquareCheck/Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+string number1 = Console.ReadLine();
+int num1 = int.Parse(number1);
+Console.Write("Введите второе число:");
+string number2 = Console.ReadLine();
+int num2 = int.Parse(number2);
+if (num1 == num2 * num2)
+{
+    Console.WriteLine ("первое число - квадрат второго");
+}
+else
+{
+   Console.WriteLine ("первое число не является квадратом второго");
+}
